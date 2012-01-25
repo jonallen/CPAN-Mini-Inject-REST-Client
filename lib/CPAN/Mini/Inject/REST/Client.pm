749dee271000f138bbd51db04dac38d0abc02041
JJ <jj@opusvl.com>
2012-01-25 13:01:05 +0000

    Documentation update

--- lib/CPAN/Mini/Inject/REST/Client.pm
+++ lib/CPAN/Mini/Inject/REST/Client.pm
@@ -12,25 +12,26 @@ use File::Spec::Functions qw/catfile/;
 
 CPAN::Mini::Inject::REST::Client - Command-line client for CPAN::Mini::Inject::REST
 
-=head1 VERSION
-
-Version 0.01
-
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+ mcpani-client <command> <options> <files>
+ 
+ mcpani-client add --host mycpan.local MyModule-0.01.tar.gz
 
-Perhaps a little code snippet.
+=head1 DESCRIPTION
 
-    use CPAN::Mini::Inject::REST::Client;
+Provides a command-line client, F<mcpani-client>, to interact with a
+L<CPAN::Mini::Inject::REST> API server.
 
-    my $foo = CPAN::Mini::Inject::REST::Client->new();
-    ...
+This allows distributions to be remotely uploaded to your CPAN mirror,
+and for the contents of your mirror to be queried and downloaded.
+
+See L<mcpani-client> for full documentation on the available commands.
 
 =cut
 
@@ -56,45 +57,6 @@ sub config_file {
 
 Jon Allen (JJ), C<< <jj at jonalen.info> >>
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-cpan-mini-inject-rest-client at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=CPAN-Mini-Inject-REST-Client>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc CPAN::Mini::Inject::REST::Client
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=CPAN-Mini-Inject-REST-Client>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/CPAN-Mini-Inject-REST-Client>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/CPAN-Mini-Inject-REST-Client>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/CPAN-Mini-Inject-REST-Client/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
 =head1 LICENSE AND COPYRIGHT
 
 Copyright 2011 Jon Allen (JJ).
@@ -104,7 +66,6 @@ under the terms of either: the GNU General Public License as published
 by the Free Software Foundation; or the Artistic License.
 
 See http://dev.perl.org/licenses/ for more information.
-
 
 =cut
 
